a090bb95765dc09d759fef841d68b7b91ac7b7b9
Kevin Wuestenbergs <KWuestenbergs@selligent.com>
2022-02-17 20:35:34 +0100

    comment tmpro

--- SelliverseWebGL/Assets/Scripts/Chat/ChatBehaviour.cs
+++ SelliverseWebGL/Assets/Scripts/Chat/ChatBehaviour.cs
@@ -1,13 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using TMPro;
+//using TMPro;
 
 public class ChatBehaviour : MonoBehaviour
 {
     [SerializeField] private GameObject chatUI = null;
-    [SerializeField] private TMP_Text chatText = null;
-    [SerializeField] private TMP_InputField inputField = null;
+    //[SerializeField] private TMP_Text chatText = null;
+    //[SerializeField] private TMP_InputField inputField = null;
 
     // Start is called before the first frame update
     void Start()
@@ -23,6 +23,6 @@ public class ChatBehaviour : MonoBehaviour
 
     private void HandleNewMessage(string message)
     {
-        chatText.text += "\n" + message;
+        //chatText.text += "\n" + message;
     }
 }
