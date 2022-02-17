dc09be0dd407afc68958ff83911718d280b86689
Rob Bertels <bertels.rob@gmail.com>
2022-02-17 17:00:52 +0100

    Boom

---
+++ Selliverse.Server/Program.cs
@@ -0,0 +1,29 @@
+﻿namespace Selliverse.Server
+{
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.Extensions.Hosting;
+    using Serilog;
+    using Serilog.Events;
+    using System.Threading.Tasks;
+
+
+    public static class Program
+    {
+        public static async Task Main(string[] args)
+        {
+            Log.Logger = new LoggerConfiguration()
+                .MinimumLevel.Debug()
+                .WriteTo.Console(LogEventLevel.Information)
+                .CreateLogger();
+
+            await Host.CreateDefaultBuilder(args)
+                .ConfigureWebHostDefaults(whb =>
+                {
+                    whb.UseStartup<Startup>();
+                })
+                .UseSerilog()
+                .Build()
+                .RunAsync();
+        }
+    }
+}
---
+++ Selliverse.Server/Startup.cs
@@ -0,0 +1,23 @@
+﻿namespace Selliverse.Server
+{
+    using Microsoft.AspNetCore.Builder;
+    using Microsoft.AspNetCore.Hosting;
+    using Microsoft.AspNetCore.Http;
+    using Microsoft.Extensions.DependencyInjection;
+
+    public class Startup
+    {
+        public void ConfigureServices(IServiceCollection serviceCollection)
+        {
+
+        }
+
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment whe)
+        {
+            app.Run(async context =>
+            {
+                await context.Response.WriteAsync("The Selliverse isn't real, it cannot hurt you.");
+            });
+        }
+    }
+}
