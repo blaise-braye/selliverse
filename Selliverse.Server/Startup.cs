86fb0eda8064000210d1c1ed158b13a6124f3de8
Blaise Braye <blaisemail@gmail.com>
2022-02-17 19:08:20 +0100

    check that ci works

--- Selliverse.Server/Startup.cs
+++ Selliverse.Server/Startup.cs
@@ -32,7 +32,7 @@
 
             app.Run(async context =>
             {
-                await context.Response.WriteAsync("The Selliverse isn't real, it cannot hurt you.");
+                await context.Response.WriteAsync("The Selliverse isn't real, it cannot hurt you. vnext");
             });
         }
     }
