d6b5c5fced4e75a8a524a4d242c5a920a73a55df
Ben <ben.beckers@gmail.com>
2022-02-17 22:33:08 +0100

    Some layout

--- Selliverse.UI/selliverse/src/app/app.module.ts
+++ Selliverse.UI/selliverse/src/app/app.module.ts
@@ -1,14 +1,18 @@
+import { CommonModule } from '@angular/common';
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { UserOverviewComponent } from './user-overview/user-overview.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    UserOverviewComponent
   ],
   imports: [
+    CommonModule,
     BrowserModule,
     AppRoutingModule
   ],
